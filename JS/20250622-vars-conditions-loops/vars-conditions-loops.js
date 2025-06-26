c6204110e4261266d542a351018955ec0dfb5cbe
Ariel <arieltanami122@gmail.com>
2025-06-26 11:12:13 +0300

    Refactor variable names for clarity in exercises 1, 2, 4, 5, 6, and 8

--- JS/20250622-vars-conditions-loops/vars-conditions-loops.js
+++ JS/20250622-vars-conditions-loops/vars-conditions-loops.js
@@ -1,17 +1,17 @@
 //ex1
-let num = 58423;
-let numStr = "" + num; // Convert num to str
+let num1 = 58423;
+let numStr = "" + num1; // Convert num to str
 
 let result = 0;
 for (let i = 0; i < numStr.length; i++) result += numStr[i] * 1; // Convert numStr[i] to num and add to result
 
-console.log(result);
+console.log(result); // 22
 
 //ex2
-let num = 10;
-let stars = "*".repeat(num);
+let num2 = 10;
+let stars = "*".repeat(num2);
 
-for (let i = 1; i <= num; i++) console.log(stars.substring(0, i));
+for (let i = 1; i <= num2; i++) console.log(stars.substring(0, i));
 
 //ex3
 let val1 = 3,
@@ -19,27 +19,27 @@ let val1 = 3,
   val3 = 1,
   val4 = 5,
   val5 = 4;
-console.log(Math.max(val1, val2, val3, val4, val5));
+console.log(Math.max(val1, val2, val3, val4, val5)); // 5
 
 //ex4
-let num = 10045;
-console.log(("" + num).length);
+let num3 = 10045;
+console.log(("" + num3).length); // 5
 
 //ex5
 let start = 5;
 let end = 13;
-for (let i = start + (start % 2); i <= end; i += 2) console.log(i);
+for (let i = start + (start % 2); i <= end; i += 2) console.log(i); // 6, 8, 10, 12
 
 //ex6
-let num = 1234;
+let num4 = 1234;
 let reversedNum = 0;
 
-while (num > 0) {
-  reversedNum = reversedNum * 10 + (num % 10);
-  num = Math.floor(num / 10);
+while (num4 > 0) {
+  reversedNum = reversedNum * 10 + (num4 % 10);
+  num4 = Math.floor(num4 / 10);
 }
 
-console.log(reversedNum);
+console.log(reversedNum); // 4321
 
 // ex7
 let depth = 5;
@@ -51,7 +51,7 @@ console.log(
   Math.floor(threeDigitNum / 100) === Math.floor(threeDigitNum % 10)
     ? "Palindrome"
     : "Not a Palindrome"
-);
+); // Palindrome
 
 //ex9
 let numberBetween1and12 = 9;
@@ -64,4 +64,4 @@ while (positiveNumber !== 1)
   console.log(
     (positiveNumber =
       positiveNumber % 2 === 0 ? positiveNumber / 2 : positiveNumber * 3 + 1)
-  );
+  ); // 5, 16, 8, 4, 2, 1
