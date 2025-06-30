cd768e96b7fb1e4ecbf6cffd4d32d10f93da7c72
Ariel <arieltanami122@gmail.com>
2025-06-30 12:57:04 +0300

    Refactor file reading functions to improve structure and readability in exe1

--- JS/20250630-async_callback/01-file-reader-with-stats.js
+++ JS/20250630-async_callback/01-file-reader-with-stats.js
@@ -2,21 +2,29 @@ import fs from "fs";
 
 let filePath = "example_file.txt";
 
-fs.readFile(filePath, "utf-8", (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-
+function readFileStats(filePath) {
   fs.stat(filePath, (err, stats) => {
     if (err) {
       console.log(err);
       return;
     }
+
+    console.log(`Size: ${stats.size} bytes`);
+    console.log("Created At:", new Date(stats.ctimeMs).toISOString());
+  });
+}
+
+function readFileWithStats(filePath) {
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
     console.log("File Content:");
     console.log(data);
-
-    console.log(`Size: ${stats.size} bytes`)
-    console.log("Created At:",(new Date(stats.ctimeMs)).toISOString());
   });
-});
+}
+
+readFileStats(filePath);
+readFileWithStats(filePath);
