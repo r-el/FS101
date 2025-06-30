5aaf8d15baca7d807436eb19feeef35d1e5731a3
Ariel <arieltanami122@gmail.com>
2025-06-30 17:35:23 +0300

    Add ES6_modules exercise2 [20250629]

---
+++ JS/20250629-ES6_modules-this/ES6_Modules/02-user-info-constants/dashboard.js
@@ -0,0 +1,4 @@
+import { MAX_USERS, DEFAULT_ROLE } from "./userConfig.js";
+
+console.log(MAX_USERS); // 100
+console.log(DEFAULT_ROLE); // "guest"
---
+++ JS/20250629-ES6_modules-this/ES6_Modules/02-user-info-constants/userConfig.js
@@ -0,0 +1,2 @@
+export const MAX_USERS = 100;
+export const DEFAULT_ROLE = "guest";
