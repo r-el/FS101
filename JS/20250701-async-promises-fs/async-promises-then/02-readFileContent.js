235b85e0e9b72b84b00fb200d138a2578c08b826
Ariel <arieltanami122@gmail.com>
2025-07-01 18:09:59 +0300

    [20250701] Exercise 2: Read a File with Promise (with .then)

---
+++ JS/20250701-async-promises-fs/async-promises-then/02-readFileContent.js
@@ -0,0 +1,18 @@
+// Exercise 2: Read a File with Promise (with .then)
+const fs = require("fs");
+
+const readFileContent = (path) =>
+  new Promise((res, rej) =>
+    fs.readFile(path, "utf8", (err, data) => (err ? rej(err) : res(data)))
+  );
+
+const filePath = process.argv[2] || "example_file.txt";
+
+readFileContent(filePath)
+  .then((content) => {
+    console.log(`Content of ${filePath}:`);
+    console.log(content);
+  })
+  .catch((err) => {
+    console.error("Error reading file:", err.message);
+  });
